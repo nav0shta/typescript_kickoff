d8846f44d9a857cd273f672b8bd003c6367f5f72
Yauheni Navosha <Yauheni_Navosha@epam.com>
2020-01-31 18:00:23 +0300

    ts basics

---
+++ app.js
@@ -0,0 +1,8 @@
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+var favouriteActivities;
+favouriteActivities = person.hobbies;
+console.log(person.name);
---
+++ app.ts
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase);
+}
